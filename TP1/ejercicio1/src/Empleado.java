98e2901bd47652d582f7e4d05a7b1d01a70191d8
ivancsir <ivanfreiberg@gmail.com>
2023-04-12 15:51:07 -0300

    tp1

---
+++ TP1/ejercicio1/src/Empleado.java
@@ -0,0 +1,51 @@
+public class Empleado {
+    String nombre;
+    String apellido;
+    int legajo;
+    int aniosTrabajados;
+
+    public Empleado(String nombre, String apellido, int legajo, int aniosTrabajados) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+        this.aniosTrabajados = aniosTrabajados;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public int getAniosTrabajados() {
+        return aniosTrabajados;
+    }
+
+    public void setAniosTrabajados(int aniosTrabajados) {
+        this.aniosTrabajados = aniosTrabajados;
+    }
+
+    public String toString() {
+        return "Nombre: " + this.nombre + "\n" + "Apellido: " + this.apellido +  "\n" + "Legajo: " + this.legajo + "\n" + "Años trabajados: " + this.aniosTrabajados;
+    }
+}
+
---
+++ TP1/ejercicio2/src/ListaGenerica.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListaGenerica<T extends Comparable<T>> {
+    private ArrayList<T> listaEmpleados;
+
+    public ListaGenerica() {
+        listaEmpleados = new ArrayList<T>();
+    }
+    public void agregar(T elemento) {
+        listaEmpleados.add(elemento);
+    }
+
+    public void obtenerLista(){
+        for (int iterador=0; iterador < listaEmpleados.size();iterador++){
+            System.out.println(listaEmpleados.toString());
+        }
+    }
+    public void tamanio() {
+        System.out.println("Tamaño de la lista: " +     listaEmpleados.size());
+    }
+
+    public void agregarElementoPrimerPosicion(T empleado){
+        listaEmpleados.add(0,empleado);
+    }
+
+    public void ordernar(){
+        Collections.sort(listaEmpleados);
+    }
+
+    public void desordenar(){
+        Collections.shuffle(listaEmpleados);
+    }
+    public T agregarElementoIndice(int indice, T empleado){
+      listaEmpleados.add(indice, empleado);
+      return (T) listaEmpleados;
+    }
+    public void devolverObjetoPosicion(int indice){
+        System.out.println( listaEmpleados.get(indice).toString());
+    }
+    public void primerElemento(){
+        int indice = 0;
+        System.out.println(listaEmpleados.get(0));
+    }
+    public void ultimoElemento(){
+        int tamanioLista = listaEmpleados.size();
+        System.out.println(listaEmpleados.get(tamanioLista-1));
+    }
+    public void borrarElemento(int indice){
+        listaEmpleados.remove(indice);
+    }
+    }
