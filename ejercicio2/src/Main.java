d343a51d63f05f4d0fb9286071574054fe76b141
ivancsir <ivanfreiberg@gmail.com>
2023-04-12 14:58:48 -0300

    ejercicio2

---
+++ ejercicio2/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaGenerica<Empleado> listaEmpleados = new ListaGenerica<Empleado>();
+        Empleado empleado1 = new Empleado("Ivan", "Freiberg",4556,50);
+        Empleado empleado2 = new Empleado("Juan", "Bolado", 4500, 10);
+        Empleado empleado3 = new Empleado("Mauro", "Maidana", 4550, 3);
+        listaEmpleados.agregar(empleado1);
+        listaEmpleados.agregar(empleado2);
+        listaEmpleados.agregar(empleado3);
+        listaEmpleados.devolverObjetoPosicion(0);
+        listaEmpleados.obtenerLista();
+        listaEmpleados.primerElemento();
+        listaEmpleados.ultimoElemento();
+        listaEmpleados.borrarElemento(0);
+        listaEmpleados.obtenerLista();
+        listaEmpleados.desordenar();
+        listaEmpleados.obtenerLista();
+        listaEmpleados.obtenerLista();
+
+        }
+    }
